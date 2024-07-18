537cb01a9d08d1626baa980b6e4ee818f2c4aa8c
LookforFPS <36205340+LookforFPS@users.noreply.github.com>
2024-07-18 21:39:19 +0200

    ✨ added gpt-4o-mini model to ChatCompletionModels

--- src/main/java/me/lookforfps/oja/aimodel/ChatCompletionModel.java
+++ src/main/java/me/lookforfps/oja/aimodel/ChatCompletionModel.java
@@ -6,23 +6,32 @@ import lombok.Getter;
 public enum ChatCompletionModel {
     /**
      * <b>Description: </b>
-     * Our most advanced, multimodal flagship model that’s cheaper and faster than GPT-4 Turbo. Currently points to gpt-4o-2024-05-13.
+     * Our high-intelligence flagship model for complex, multi-step tasks.
+     * GPT-4o is cheaper and faster than GPT-4 Turbo.
      */
     GPT_4_O(new Model("gpt-4o")),
     /**
      * <b>Description: </b>
-     * The latest GPT-4 Turbo model with vision capabilities. Vision requests can now use JSON mode and function calling. Currently points to gpt-4-turbo-2024-04-09.
+     * Our affordable and intelligent small model for fast, lightweight tasks.
+     */
+    GPT_4_O_MINI(new Model("gpt-4o-mini")),
+    /**
+     * <b>Description: </b>
+     * The latest GPT-4 Turbo model with vision capabilities.
+     * Vision requests can now use JSON mode and function calling.
      */
     GPT_4_TURBO(new Model("gpt-4-turbo")),
     /**
      * <b>Description: </b>
      * Currently points to gpt-4-0613.
+     * Snapshot of gpt-4 from June 13th 2023 with improved function calling support.
      */
     GPT_4(new Model("gpt-4")),
 
     /**
      * <b>Description: </b>
-     * Currently points to gpt-3.5-turbo-0125.
+     * The latest GPT-3.5 Turbo model with higher accuracy at responding in requested formats and a fix for a bug which caused a text encoding issue for non-English language function calls.
+     * Returns a maximum of 4,096 output tokens.
      */
     GPT_3_5_TURBO(new Model("gpt-3.5-turbo"));
 
