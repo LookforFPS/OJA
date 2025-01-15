a2e55e8d967119505080785c8e2a7a75bd5d8d05
LookforFPS <36205340+LookforFPS@users.noreply.github.com>
2025-01-15 18:29:39 +0100

    ♻️ modified SystemMessage to support multiple content parts

--- src/main/java/me/lookforfps/oja/chatcompletion/model/natives/message/SystemMessage.java
+++ src/main/java/me/lookforfps/oja/chatcompletion/model/natives/message/SystemMessage.java
@@ -3,21 +3,29 @@ package me.lookforfps.oja.chatcompletion.model.natives.message;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import me.lookforfps.oja.chatcompletion.model.natives.message.content.Content;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class SystemMessage extends Message {
 
-    private String content;
+    private Content content;
     private String name;
 
-    public SystemMessage(String content, String name) {
-        super(MessageRole.SYSTEM);
+    public SystemMessage(Content content, String name) {
+        super(MessageRole.DEVELOPER);
         this.content = content;
         this.name = name;
     }
 
+    public SystemMessage(String content, String name) {
+        this(Content.createTextContent(content), name);
+    }
+
+    public SystemMessage(Content content) {
+        this(content, null);
+    }
     public SystemMessage(String content) {
         this(content, null);
     }
